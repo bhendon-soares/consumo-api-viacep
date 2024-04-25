c3db5c001b086b64378c1d566bb6f2dc09a0fb55
Bhendon Soares <bhendon.soares12@gmail.com>
2024-04-25 12:39:33 -0300

    feat: adiciona estrutura do projeto spring

---
+++ src/test/java/com/bhendonsoares/consumoapiviacep/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhendonsoares.consumoapiviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
