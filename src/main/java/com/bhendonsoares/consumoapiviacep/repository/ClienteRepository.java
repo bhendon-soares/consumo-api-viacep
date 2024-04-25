4feabeb968fc99ef7301688dc278cf0a87214448
Bhendon Soares <bhendon.soares12@gmail.com>
2024-04-25 12:54:47 -0300

    feat: adiciona repository de cliente e endereco

---
+++ src/main/java/com/bhendonsoares/consumoapiviacep/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.bhendonsoares.consumoapiviacep.repository;
+
+import com.bhendonsoares.consumoapiviacep.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
